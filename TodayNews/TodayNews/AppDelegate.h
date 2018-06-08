e9052a45151a4b249aa391a49be3e85c779ca50b
Franky's iMac <548913086@qq.com>
2018-06-08 17:51:06 +0800

    初始化项目

---
+++ TodayNews/TodayNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TodayNews
+//
+//  Created by Franky on 2018/6/8.
+//  Copyright © 2018年 Franky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TodayNews/TodayNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TodayNews
+//
+//  Created by Franky on 2018/6/8.
+//  Copyright © 2018年 Franky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
